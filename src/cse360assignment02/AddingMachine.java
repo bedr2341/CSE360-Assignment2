974ca1079426e1955712a80edf7f75a88ec32ead
Bader Alrifai <bedrrifai1@gmail.com>
2020-10-03 22:40:06 -0700

    Created the project, the CSE360Assigment02 package, and the AddingMachine class with javadoc comments.

---
+++ src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,47 @@
+package cse360assignment02;
+
+public class AddingMachine {
+	private int total;
+
+	/**
+	 * Constructs an instance of the AddingMachine class and instnatiates the variable total to 0
+	 */
+	public AddingMachine() {
+		total = 0;  // not needed - included for clarity
+	}
+
+	/**
+	 * @return the value of total
+	 */
+	public int getTotal() {
+		return 0;
+	}
+
+	/**
+	 * Adds the parameter to the total variable
+	 * @param value is added to total
+	 */
+	public void add(int value) {
+	}
+
+	/**
+	 * Subtracts the parameter from the total variable
+	 * @param value is subtracted from total
+	 */
+	public void subtract(int value) {
+	}
+
+	/**
+	 * @return A history of the transactions
+	 */
+	public String toString() {
+		return "";
+	}
+
+	/**
+	 * Clears the transactions stored
+	 */
+	public void clear() {
+
+	}
+}
