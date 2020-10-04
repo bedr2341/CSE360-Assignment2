6c5ddc58487cbfc77645db0af068116fbf1bda55
Bader Alrifai <bedrrifai1@gmail.com>
2020-10-03 22:53:39 -0700

    Wrote the code for each functions, added the log variable to keep track of transaction, and then updated the javadocs accordingly.

--- src/cse360assignment02/AddingMachine.java
+++ src/cse360assignment02/AddingMachine.java
@@ -2,46 +2,57 @@ package cse360assignment02;
 
 public class AddingMachine {
 	private int total;
+	private String log;
 
 	/**
-	 * Constructs an instance of the AddingMachine class and instnatiates the variable total to 0
+	 * Constructs an instance of the AddingMachine class and instnatiates the variable total to 0 and the log to "0"
 	 */
 	public AddingMachine() {
-		total = 0;  // not needed - included for clarity
+		this.total = 0;  // not needed - included for clarity
+		this.log = "0";
 	}
 
 	/**
 	 * @return the value of total
 	 */
 	public int getTotal() {
-		return 0;
+		return this.total;
 	}
 
 	/**
 	 * Adds the parameter to the total variable
+	 * Updates the transaction
 	 * @param value is added to total
 	 */
 	public void add(int value) {
+		this.total += value;
+		this.log += " + ";
+		this.log += value;
 	}
 
 	/**
 	 * Subtracts the parameter from the total variable
+	 * Updates the transaction
 	 * @param value is subtracted from total
 	 */
 	public void subtract(int value) {
+		this.total -= value;
+		this.log += " - ";
+		this.log += value;
 	}
 
 	/**
 	 * @return A history of the transactions
 	 */
 	public String toString() {
-		return "";
+		return this.log;
 	}
 
 	/**
-	 * Clears the transactions stored
+	 * Resets the value of total and log
 	 */
 	public void clear() {
-
+		this.total = 0;
+		this.log = "0";
 	}
 }
